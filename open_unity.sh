14f27141464106d4d651e5b98090e616029ddb93
Yosapol Jitrak <yosapoljitrak@Yosapols-MacBook-Pro.local>
2018-03-29 15:57:16 +0700

    [Mod]: open_unity.sh - Add macOS                      - Choice build target comeback.

--- open_unity.sh
+++ open_unity.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+
+buildTarget=''
+if [[ $1 == '-a' ]] || [[ $1 == '--android' ]] || [[ $1 == '--Android' ]]; then
+    echo "Build target is Android"
+    buildTarget='Android'
+fi
+
+if [[ $1 == '-i' ]] || [[ $1 == '--ios' ]] || [[ $1 == '--iOS' ]]; then
+    echo "Build target is iOS"
+    buildTarget='iOS'
+fi
+
+buildTargetExtend=''
+
+if [[ $buildTarget != '' ]]; then
+    buildTargetExtend="-buildTarget $buildTarget"
+fi
+
 set -e
 if [[ $(uname -r) =~ Microsoft$ ]]; then
-    '/mnt/c/Program Files/Unity/Editor/Unity.exe'
+    '/mnt/c/Program Files/Unity/Editor/Unity.exe' $buildTargetExtend
 else
     case "$(uname -s)" in
     
        Darwin)
-         echo 'Mac OS X'
+         /Applications/Unity/Unity.app/Contents/MacOS/Unity $buildTargetExtend
          ;;
     
        Linux)
